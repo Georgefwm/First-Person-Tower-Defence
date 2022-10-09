36e66e3db8ef5995d366d3c106df053127a20ec6
George McLachlan <georgemclachlan2@gmail.com>
2022-10-09 22:33:00 +1300

    Updated Mesh1P position

--- MyProject/Player/PlayerCharacter.cpp
+++ MyProject/Player/PlayerCharacter.cpp
@@ -39,8 +39,8 @@ APlayerCharacter::APlayerCharacter()
 	Mesh1P->SetupAttachment(FirstPersonCameraComponent);
 	Mesh1P->bCastDynamicShadow = false;
 	Mesh1P->CastShadow = false;
-	Mesh1P->SetRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
-	Mesh1P->SetRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
+	Mesh1P->SetRelativeRotation(FRotator(7.3f, -23.19f, 5.02f));
+	Mesh1P->SetRelativeLocation(FVector(26.67f, 6.31f, -158.7f));
 
 	//// Server ////
 	
