bc108cce178f61acf008927391f495331aac06a6
George McLachlan <georgemclachlan2@gmail.com>
2022-11-13 15:24:25 +1300

    Added struct for data table implementation

---
+++ MyProject/Buildings/BuildingStats.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Engine/DataTable.h"
+#include "BuildingStats.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBuildingStats : public FTableRowBase
+{
+	
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(BlueprintReadWrite)
+	int MaxHp;
+
+	UPROPERTY(BlueprintReadWrite)
+	float AttackRange;
+
+	UPROPERTY(BlueprintReadWrite)
+	int AttackDamage;
+	
+	UPROPERTY(BlueprintReadWrite)
+	float AttackSpeed;
+
+	UPROPERTY(BlueprintReadWrite)
+	float BuildTime;
+};
