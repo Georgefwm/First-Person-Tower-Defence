bdcad58e2c684f7abfe4aba2165116f7ea70386c
George McLachlan <georgemclachlan2@gmail.com>
2022-11-15 13:19:08 +1300

    Added generic move slow modifier

---
+++ MyProject/ModifierEffects/SubTypes/MoveSlowModifier.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MoveSlowModifier.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MyProject/ModifierEffects/ModiferComponent.h"
+
+AMoveSlowModifier::AMoveSlowModifier()
+{
+	ModifierValue = 0.1;
+	Duration = 5.0;
+}
+
+FString AMoveSlowModifier::GetDescription()
+{
+	return Super::GetDescription();
+
+}
+
+void AMoveSlowModifier::Apply(AEnemy* Enemy, UModiferComponent* ModiferComponent)
+{
+	if (!ModiferComponent->ActiveModifiers.IsEmpty())
+	{
+		for (int Index = 0; Index < ModiferComponent->ActiveModifiers.Num(); Index++)
+		{
+			if (!ModiferComponent->ActiveModifiers.IsValidIndex(Index))
+				continue;
+			
+			AModifier* Mod = ModiferComponent->ActiveModifiers[Index];
+			
+			if (Mod->IsA(GetClass()))
+			{
+				GetWorldTimerManager().ClearTimer(Mod->Timer);
+				ModiferComponent->ActiveModifiers.Remove(Mod);
+				Mod->Destroy();
+			}
+		}
+	}
+	
+	Target = Enemy;
+	OwningComponent = ModiferComponent;
+
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, Target->GetActorNameOrLabel());
+
+	Target->SetMoveSpeed(Target->GetBaseMoveSpeed() * ModifierValue);
+
+	GetWorldTimerManager().SetTimer(Timer, this, &AMoveSlowModifier::Remove, Duration, false);
+
+	ModiferComponent->ActiveModifiers.Add(this);
+}
+
+void AMoveSlowModifier::Remove()
+{
+	if (Target && OwningComponent)
+	{
+		Target->SetMoveSpeed(Target->GetBaseMoveSpeed());
+		OwningComponent->ActiveModifiers.Remove(this);
+	}
+	Destroy();
+}
---
+++ MyProject/ModifierEffects/SubTypes/MoveSlowModifier.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyProject/ModifierEffects/Modifier.h"
+#include "MoveSlowModifier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AMoveSlowModifier : public AModifier
+{
+	GENERATED_BODY()
+
+	AMoveSlowModifier();
+
+	// Returns description of modifier
+	virtual FString GetDescription() override;
+
+	// Apply modifier to an Enemy
+	virtual void Apply(AEnemy* Target, UModiferComponent* ModiferComponent) override;
+
+	// Used by timer to self-remove modifier
+	virtual void Remove() override;
+};
