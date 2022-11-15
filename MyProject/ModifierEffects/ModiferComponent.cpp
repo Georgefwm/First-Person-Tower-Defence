dff7b6b868569f50b8a02a1c5eb4933eb5cb8473
George McLachlan <georgemclachlan2@gmail.com>
2022-11-15 13:18:34 +1300

    Added Modifier component to handle an enemy's modifiers

---
+++ MyProject/ModifierEffects/ModiferComponent.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ModiferComponent.h"
+
+#include "ComponentUtils.h"
+
+
+void UModiferComponent::GetModifiers(TArray<AModifier*> Modifiers)
+{
+	
+}
+
+// Called when the game starts
+void UModiferComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+
+// Called every frame
+void UModiferComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                      FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
+void UModiferComponent::ApplyModifier(UClass* ModClass)
+{	
+	FActorSpawnParameters const SpawnParameters;
+	
+	if (AModifier* Modifier = GetWorld()->SpawnActor<AModifier>(ModClass, SpawnParameters))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, TEXT("Slowing enemy"));
+
+		if (AEnemy* Owner = StaticCast<AEnemy*>(GetOwner()))
+			Modifier->Apply(Owner, this);
+	}
+	
+}
+
---
+++ MyProject/ModifierEffects/ModiferComponent.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Modifier.h"
+#include "Components/ActorComponent.h"
+#include "ModiferComponent.generated.h"
+
+/**
+ * Manages modifiers of the attached actor
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class MYPROJECT_API UModiferComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+
+	UPROPERTY(VisibleAnywhere)
+	TArray<AModifier*> ActiveModifiers;
+
+	UFUNCTION()
+	void GetModifiers(TArray<AModifier*> Modifiers);
+
+	UPROPERTY(VisibleAnywhere)
+	AEnemy* OwningEnemy;
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+
+	// Takes the class of the modifier to be created
+	UFUNCTION()
+	void ApplyModifier(UClass* ModClass);
+	
+
+	
+};
