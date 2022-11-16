cb5105b9654607862ff2173df426127396895158
George McLachlan <georgemclachlan2@gmail.com>
2022-11-16 14:54:01 +1300

    Added Burn modifier

---
+++ MyProject/ModifierEffects/SubTypes/BurnModifier.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BurnModifier.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "MyProject/ModifierEffects/ModiferComponent.h"
+
+ABurnModifier::ABurnModifier()
+{
+	ModifierValue = 0.1;
+	Duration = 5.0;
+}
+
+FString ABurnModifier::GetDescription()
+{
+	return Super::GetDescription();
+
+}
+
+void ABurnModifier::Apply(AEnemy* Enemy, UModiferComponent* ModiferComponent)
+{
+	if (!ModiferComponent->ActiveModifiers.IsEmpty())
+	{
+		for (int Index = 0; Index < ModiferComponent->ActiveModifiers.Num(); Index++)
+		{
+			if (!ModiferComponent->ActiveModifiers.IsValidIndex(Index))
+				continue;
+			
+			AModifier* Mod = ModiferComponent->ActiveModifiers[Index];
+			
+			if (Mod->IsA(GetClass()))
+			{
+				GetWorldTimerManager().ClearTimer(Mod->Timer);
+				ModiferComponent->ActiveModifiers.Remove(Mod);
+				Mod->Destroy();
+			}
+		}
+	}
+	
+	Target = Enemy;
+	OwningComponent = ModiferComponent;
+
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, Target->GetActorNameOrLabel());
+
+	Target->SetMoveSpeed(Target->GetBaseMoveSpeed() * ModifierValue);
+
+	GetWorldTimerManager().SetTimer(Timer, this, &ABurnModifier::Remove, Duration, true);
+
+	ModiferComponent->ActiveModifiers.Add(this);
+}
+
+void ABurnModifier::Remove()
+{
+	if (Target && OwningComponent)
+	{
+		OwningComponent->ActiveModifiers.Remove(this);
+	}
+	Destroy();
+}
---
+++ MyProject/ModifierEffects/SubTypes/BurnModifier.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+
+#include "CoreMinimal.h"
+#include "MyProject/ModifierEffects/Modifier.h"
+#include "BurnModifier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API ABurnModifier : public AModifier
+{
+	GENERATED_BODY()
+
+	ABurnModifier();
+
+	// Returns description of modifier
+	virtual FString GetDescription() override;
+
+	// Apply modifier to an Enemy
+	virtual void Apply(AEnemy* Target, UModiferComponent* ModiferComponent) override;
+
+	// Used by timer to self-remove modifier
+	virtual void Remove() override;
+};
