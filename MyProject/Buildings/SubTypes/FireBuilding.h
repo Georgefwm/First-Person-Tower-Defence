97971722f01ebd7fa7c152993f021516426a1170
George McLachlan <georgemclachlan2@gmail.com>
2022-11-17 22:08:44 +1300

    Added initial fire building class (does fire damage)

---
+++ MyProject/Buildings/SubTypes/FireBuilding.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../Building.h"
+#include "FireBuilding.generated.h"
+
+UCLASS()
+class MYPROJECT_API AFireBuilding : public ABuilding
+{
+	GENERATED_BODY()
+
+	
+
+public:
+	// Sets default values for this actor's properties
+	AFireBuilding();
+	
+	UPROPERTY(Category=Actor, EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* BaseModel;
+	
+	virtual FVector GetSearchPosition() override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void Build(float DeltaTime) override;
+
+	virtual void Attack(float DeltaTime) override;
+
+	virtual void CheckForNewTarget() override;
+};
