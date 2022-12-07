763d888418941d3b686ac064511ad400823a90a0
George McLachlan <georgemclachlan2@gmail.com>
2022-12-07 18:04:52 +1300

    Added Barrier building

---
+++ MyProject/Buildings/SubTypes/ABarrierBuilding.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABarrierBuilding.h"
+
+ABarrierBuilding::ABarrierBuilding()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BuildingName = FString("BarrierBuilding");
+
+	// Import stats from building data table
+	Super::SetupStats();
+}
+
+FVector ABarrierBuilding::GetSearchPosition()
+{
+	return Super::GetSearchPosition();
+}
+
+void ABarrierBuilding::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABarrierBuilding::Tick(float DeltaTime)
+{
+	switch (CurrentBuildingState)
+	{
+	case (EBuildingState::BS_Placing):		{ UpdatePreview();							} break;
+	case (EBuildingState::BS_Building):		{ Build(DeltaTime);							} break;
+	case (EBuildingState::BS_Idle):			{											} break;
+	case (EBuildingState::BS_Attacking):	{											} break;
+	case (EBuildingState::BS_Disabled):		{											} break;
+	default: return;
+	}
+}
+
+void ABarrierBuilding::Build(float DeltaTime)
+{
+	Super::Build(DeltaTime);
+}
+
+void ABarrierBuilding::Attack(float DeltaTime)
+{
+}
+
+void ABarrierBuilding::CheckForNewTarget()
+{
+}
---
+++ MyProject/Buildings/SubTypes/ABarrierBuilding.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyProject/Buildings/Building.h"
+#include "ABarrierBuilding.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API ABarrierBuilding : public ABuilding
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABarrierBuilding();
+	
+	virtual FVector GetSearchPosition() override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	virtual void Build(float DeltaTime) override;
+
+	virtual void Attack(float DeltaTime) override;
+
+	virtual void CheckForNewTarget() override;
+};
