df1e3f31413cc81ace260f63c007a7904b1f2c92
George McLachlan <georgemclachlan2@gmail.com>
2022-12-12 18:22:54 +1300

    Implemented comparison based on distance

---
+++ MyProject/Buildings/Comparators/SubTypes/ClosestPriority.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClosestPriority.h"
+
+UClosestPriority::UClosestPriority()
+{
+	Name = "Closest";
+}
+
+AEnemy* UClosestPriority::Compare(ABuilding *Asker, TArray<AActor*> &Candidates)
+{
+	AEnemy* BestTarget = nullptr;
+	float ShortestDistance = 10000;
+	
+	// Always try to attack the closet enemy
+	for (AActor* Actor : Candidates)
+	{
+		// Might be redundant check
+		if (AEnemy* Enemy = Cast<AEnemy>(Actor))
+		{
+			if (!IsValid(Enemy))
+				continue;
+
+			if (Enemy->IsDead)
+				continue;
+
+			float Distance = FVector::Dist(Asker->GetSearchPosition(), Enemy->GetActorLocation());
+
+			if (Distance < ShortestDistance)
+			{
+				BestTarget = Enemy;
+				ShortestDistance = Distance;
+			}
+		}
+	}
+	
+	return BestTarget;
+}
