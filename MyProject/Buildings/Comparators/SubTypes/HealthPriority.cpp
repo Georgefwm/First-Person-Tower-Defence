215c353c25750c23a73bc480b93e265c2a252333
George McLachlan <georgemclachlan2@gmail.com>
2022-12-12 18:22:46 +1300

    Implemented comparison based on health

---
+++ MyProject/Buildings/Comparators/SubTypes/HealthPriority.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthPriority.h"
+
+UHealthPriority::UHealthPriority()
+{
+	Name = "Highest HP";
+}
+
+AEnemy* UHealthPriority::Compare(ABuilding *Asker, TArray<AActor*> &Candidates)
+{
+	AEnemy* BestTarget = nullptr;
+	int HighestHealth = 0;
+
+	// Always try to attack the closet enemy
+	for (AActor* Actor : Candidates)
+	{
+		// Might be redundant check
+		if (AEnemy* Enemy = Cast<AEnemy>(Actor))
+		{
+			if (!IsValid(Enemy))
+				continue;
+
+			if (Enemy->IsDead)
+				continue;
+
+			int CandidateHealth = Enemy->GetHP();
+
+			if (CandidateHealth > HighestHealth)
+			{
+				BestTarget = Enemy;
+				HighestHealth = CandidateHealth;
+			}
+		}
+	}
+	
+	return BestTarget;
+}
---
+++ MyProject/Buildings/Comparators/SubTypes/HealthPriority.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyProject/Buildings/Comparators/PrioritySystem.h"
+#include "HealthPriority.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UHealthPriority : public UPrioritySystem
+{
+	GENERATED_BODY()
+
+public:
+	UHealthPriority();
+	
+	virtual AEnemy* Compare(ABuilding *Asker, TArray<AActor*> &Candidates) override;
+};
